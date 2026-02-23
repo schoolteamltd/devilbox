ee046138928ba2575a8f0ec426e219969eb5da07
Joshua Tam <297250+joshuatam@users.noreply.github.com>
2026-02-23 12:56:43 +0800

    add local run config

--- shell.sh
+++ shell.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if hash docker-compose 2>/dev/null; then
-	docker-compose exec --user devilbox php bash -l 
+	docker-compose exec --user devilbox php bash -c "source /opt/nvm/nvm.sh && sudo chmod a+w /opt/nvm && nvm install 16 && nvm use 16" -l 
+	docker-compose exec --user devilbox php bash -l
 else
-	docker compose exec --user devilbox php bash -l
+	docker compose exec --user devilbox php bash -l 
 fi
